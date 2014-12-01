13992a4026defbad6d60738050942720fa3d774d
james-norcross <james.norcross@gmail.com>
2014-12-01 14:22:58 -0600

    initial commit

---
+++ src/projecteuler/prob25/FibonacciNum.java
@@ -0,0 +1,16 @@
+package projecteuler.prob25;
+
+public class FibonacciNum {
+
+	/**
+	 * Author: James Norcross
+	 * Date: 12/1/14
+	 * Purpose: Project Euler Problem 25
+	 * Description: Calculates the terms in the Fibonacci sequence and returns the first term with 1000 digits
+	 */
+	public static void main(String[] args) {
+
+		
+	}
+
+}
